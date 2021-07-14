56648f27684698c8d82d96f9737e401f6a903b54
luckykumar2021 <79297404+luckykumar2021@users.noreply.github.com>
2021-07-14 16:01:45 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -18,33 +18,4 @@ public class AppTest
         assertTrue( true );
     }
     
-    @Test
-    public void shouldAnswerWithTrue(1)
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue(2)
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue(3)
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue(4)
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue(5)
-    {
-        assertTrue( true );
-    }
 }
